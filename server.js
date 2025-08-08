5c2034b186ae8b247f1db0d42caf0f8c611378bb
zinhle <ngwanejeanette3@gmail.com>
2025-08-09 00:18:31 +0200

    installed nodemon

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+const app = express();
 
 
 app.use((cors));
 app.use((express));
 
 app.listen(PORT, () => {
-  //what the server should run on the PORT.
+console.log('server has started')
 });
 
