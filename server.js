63c55ad35018c400c90a8aae22a92faa06650e8d
zinhle <ngwanejeanette3@gmail.com>
2025-09-16 23:52:43 +0200

    edit delete and testing on postman

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const blogs = require("./routes/blogs").default;
 app.use(cors());
 app.use(express.json());
 
+
+
 // to get content of a specific blog from the front end
 // do i need to use [1] to get specific content, if i have my blog information as an array of objects?
 app.get("/", (req, res) => {
@@ -20,26 +22,30 @@ app.get("/", (req, res) => {
 // create a new blog
 app.post("/createBlog", (req, res) => {
   const { title, category, content } = req.body;
+
   if (!title || !category || !content) {
-    res.json("all fields required");
-  } else {
-    const newBlog = {
-      id: blogs.length + 1,
-      title,
-      category,
-      content,
-    };
+    return res.status(400).json({ error: "All fields required" }); // stop execution here
   }
+
+  const newBlog = {
+    id: blogs.length + 1,
+    title,
+    category,
+    content,
+  };
+
   blogs.push(newBlog);
+
+  res.status(201).json(newBlog);
 });
 
+
 // delete a blog
-app.delete("/api/blogs/:id", (req, res) => {
-  const blogIndex = blogs.findIndex((b) => b.id === parseInt(req.params.id));
+app.delete("/api/blogs/:index", (req, res) => {
+  const blogIndex = blogs.findIndex((b) => b.index === parseInt(req.params.index));
   if (blogIndex === -1) {
     return res.status(404).json({ error: "Blog not found" });
   }
-
   const deleted = blogs.slice(blogIndex, 1);
   res.json({ message: "Blog deleted", blog: deleted[0] });
 });
