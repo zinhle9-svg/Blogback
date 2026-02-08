5525936886e782fae9664ec4bb836cec104b5904
zinhle <ngwanejeanettte3@gmail.com>
2026-02-08 21:47:34 +0200

    updated npm and installed multer

--- routes/create.js
+++ routes/create.js
@@ -3,7 +3,8 @@ const express = require("express");
 const router = express.Router();
 
 
-router.post("/", (req, res) => {
+router.post("/upload", (req, res) => {
+  res.send("File uploaded successfully");
   console.log(req);
   const newBlog = {
     id: Blogs.length + 1,
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
+const path = require('path');
+const multer = require('multer');
+
 
 app.use(cors());
 app.use(bodyParser.json());
