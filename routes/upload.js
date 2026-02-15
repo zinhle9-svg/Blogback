925ff96eff64a70c26c349a29169cb2e50c26edc
zinhle <ngwanejeanettte3@gmail.com>
2026-02-15 20:18:38 +0200

    matching the file name that is being uploaded.

--- routes/upload.js
+++ routes/upload.js
@@ -4,7 +4,10 @@ const upload = multer({ dest: 'public/images/' })
 const router = express.Router();
 
 
-router.post('/public/images', upload.single('image'), (req, res) => {
+router.post('/api/upload', (req,res ) => {
+
+})
+router.post('/public/images', upload.single('pic1'), (req, res) => {
   res.send('File uploaded successfully')
 })
 
